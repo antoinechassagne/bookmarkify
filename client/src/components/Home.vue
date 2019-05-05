5cc803c5809c84e00a9893ac67f8e1645ba74051
Antoine Chassagne <antoine.chassagne@protonmail.com>
2019-05-05 19:46:50 +0200

    Add client configuration for API connection + Home component

---
+++ client/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      {{ message }}
+    </h1>
+    <p>
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      message: 'Welcome to Bookmarkify',
+      description: 'Bookmarkify is an app that allows you to save useful bookmarks that you want to keep.'
+    }
+  }
+}
+</script>
--- client/src/services/BookmarksService.js
+++ client/src/services/BookmarksService.js
@@ -1,1 +1,11 @@
 import Api from '@/services/Api'
+
+export default {
+  fetchBookmarks () {
+    return Api().get('bookmarks')
+  },
+
+  addBookmark (params) {
+    return Api().post('bookmarks/add', params)
+  }
+}
