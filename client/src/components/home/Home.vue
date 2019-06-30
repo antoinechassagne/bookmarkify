4299f2e21ce505797de02b440768156d2f8c4a92
Antoine Chassagne <antoine.chassagne@protonmail.com>
2019-06-30 21:30:51 +0200

    Add style on Home component

--- client/src/components/home/Home.vue
+++ client/src/components/home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <h1>
+  <div id="vueHome" class="text-center">
+    <h1 class="mt-20 mb-10 text-6xl">
       {{ title }}
     </h1>
     <p>
       {{ description }}
     </p>
-    <router-link v-bind:to="{ name: 'Bookmarks' }">
+    <router-link v-bind:to="{ name: 'Bookmarks' }" class="button mt-5 shadow-2xl">
       Let's start now !
     </router-link>
   </div>
@@ -22,4 +22,18 @@ export default {
     };
   },
 };
-</script>`
+</script>
+
+<style lang="scss">
+  .button {
+    display: inline-block;
+    border: solid 1px black;
+    border-radius: 50px;
+    padding: 10px;
+    transition: all 0.3s;
+    &:hover {
+      background-color: black;
+      color: white;
+     }
+  }
+</style>
