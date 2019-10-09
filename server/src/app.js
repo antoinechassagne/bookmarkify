6c8a06fa9311e928ea8964ed9b6918b38c8755c3
Antoine Chassagne <antoine@webians.fr>
2019-10-09 14:35:32 +0200

    fix typo

--- server/src/app.js
+++ server/src/app.js
@@ -25,5 +25,5 @@ app.use('/api', bookmark);
 
 const port = process.env.PORT || 8081;
 app.listen(port, () => {
-    console.log('Server launched on port' port);
+    console.log('Server launched on port', port);
 });
